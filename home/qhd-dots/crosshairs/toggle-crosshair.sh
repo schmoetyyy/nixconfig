d9589d6bcf37529cb26494d10c81aee8f3b42635
schmoetyyy <natkarn@proton.me>
2026-08-18 21:34:41 +0200

    toggle-crosshair.sh aktualisieren

--- home/qhd-dots/crosshairs/toggle-crosshair.sh
+++ home/qhd-dots/crosshairs/toggle-crosshair.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# -f sucht im gesamten Befehlsaufruf (also auch im Pfad), nicht nur im Prozessnamen
+
 if pgrep -f "wl-crosshair" > /dev/null; then
-    # Wenn es läuft -> Töte den Prozess (auch hier ohne -x, sondern mit -f)
     pkill -f "wl-crosshair"
 else
-    # Wenn es nicht läuft -> Starte es mit dem Bild im Hintergrund (&)
     wl-crosshair /home/schmoetyyy/nixconfig/home/qhd-dots/crosshairs/cross5.png &
 fi
